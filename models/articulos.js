f82b5dd8e1502b10079bfb77c51c5387cc219a61
juan <juanpereira.liza@gmail.com>
2024-10-29 09:32:52 -0500

    commit 1e3f3

---
+++ models/articulos.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const articuloSchema = new mongoose.Schema({
+    nombre: { type: String, required: true, maxlength: 50 },
+    precio: { type: Number, required: true },
+    stock: { type: Number, required: true },
+    categoria: { type: mongoose.Schema.Types.ObjectId, ref: 'categoria', required: true },
+    estado: { type: Number, default: 1 }, //1 activo 0 inactivo
+},{
+    timestamps: true,
+})
+
+export default articuloSchema
---
+++ models/categoria.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const categoriaSchema = new mongoose.Schema({
+    descripcion: { type: String, required:true},
+    estado: { type: String, default: 1}, //1 activo 0 inactivo
+},{
+    timestamps: true,
+})
+
+export default categoriaSchema
