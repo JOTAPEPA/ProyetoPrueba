2b36a74a68f0f67e5ee79b5689382e65e4610f98
Your Name <you@example.com>
2024-10-31 08:16:21 -0500

    routes-controllers

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import httpTerceros from './routes/terceros.js'
 
 const app = express();
+app.use(express.json());
+app.use("/terceros", httpTerceros);
 
 app.listen(process.env.PORT,()=>{
     console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
