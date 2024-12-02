7c9387d046689e34736f2d471d3b440b547a2d4a
Your Name <you@example.com>
2024-12-02 16:08:56 -0500

    correcion cors

--- index.js
+++ index.js
@@ -13,14 +13,19 @@ import cors from 'cors';
 
 
 const app = express();
-app.use(cors(
-    {
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'x-token'],
-        credentials: true
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
-));
+  },
+  credentials: true, // Si estás enviando cookies o encabezados personalizados
+}));
 app.use(express.json());
 app.use("/api/terceros", httpTerceros);
 app.use("/api/movimientos", httpMovimiento);
